cb0eb7d9369900bae6674e902d92b765dc290ff6
Violeta Fiel Lawrence (ERNI) <fiel.violeta@betterask.erni>
2021-03-12 11:46:51 +0800

    Added search

--- src/app/components/products/list/list.component.ts
+++ src/app/components/products/list/list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/shared/services/products.service';
 import { ProductListModel } from 'src/app/shared/models/products/product-list.model';
-import { ProductsModule } from '../products.module';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -10,14 +9,16 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  data: ProductsModule;
+  data: ProductListModel;
   apiBaseUrl = environment.app_url;
   searchText: string = '';
+  dataTmp: ProductListModel;
 
   constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
     this.productsService.getProductList().then(res => {
+      this.dataTmp = res;
       this.data = res;
     }).catch((err) => {
       console.log('It failed!', err);
@@ -26,5 +27,16 @@ export class ListComponent implements OnInit {
 
   onImgError(event){
     event.target.src = './assets/icons/prodgr_default_300.png';
+  }
+  
+  onChange(value) {
+    const data: any = this.data;
+    if(value) {
+      this.data = data.filter(res => {
+        return res.name.toLowerCase().includes(value);
+      });
+    } else {
+      this.data = this.dataTmp;
+    }
   }
 }
